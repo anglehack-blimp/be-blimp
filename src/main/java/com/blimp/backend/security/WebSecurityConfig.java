64b3ab5eaf6c53e84f09960060ae83be9ebd1e46
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-07-28 01:23:49 +0700

    update: remove typo

--- src/main/java/com/blimp/backend/security/WebSecurityConfig.java
+++ src/main/java/com/blimp/backend/security/WebSecurityConfig.java
@@ -1,11 +1,9 @@
 package com.blimp.backend.security;
 
 import com.blimp.backend.service.UserService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
